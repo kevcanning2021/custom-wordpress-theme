6b3294b9fda2dd0d32392c104ff13aa2b8fffe74
Kevin Canning <kevin@quazar.co.za>
2022-11-02 08:26:54 +0200

    Make page titles dynamic.

--- footer.php
+++ footer.php
@@ -1,3 +1,7 @@
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+           <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+       </footer>    
     <?php wp_footer(); ?>
 </body>
 </html>
